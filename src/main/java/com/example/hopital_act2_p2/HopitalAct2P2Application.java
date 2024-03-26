bb07e1e9077e0ce3088b77d5e3f6e4da284bd405
Amine Ouzane <amineouzane10@gmail.com>
2024-03-26 16:08:16 +0000

    First Commit

---
+++ src/main/java/com/example/hopital_act2_p2/HopitalAct2P2Application.java
@@ -0,0 +1,13 @@
+package com.example.hopital_act2_p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopitalAct2P2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopitalAct2P2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hopital_act2_p2/HopitalAct2P2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hopital_act2_p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopitalAct2P2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
